3d956578c8a054fed00f81914460ea41d098aee8
Antoni Avalos <frankantony_2001@hotmail.com>
2022-09-05 12:49:05 -0500

    Str y Suma

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString ='Antoni';
+const nuevaString ='Tony';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
@@ -28,14 +28,14 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  suma(x+y);
 }
 
 function resta(x, y) {
